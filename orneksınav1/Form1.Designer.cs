e840a58d000f385f3ac9e5f8348e5a21fc20a32b
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-12-11 13:31:02 +0300

    Add files via upload

---
+++ orneksınav1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace orneksınav1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnbuyuknot = new System.Windows.Forms.Button();
+            this.btnEndusuknot = new System.Windows.Forms.Button();
+            this.btnBelge = new System.Windows.Forms.Button();
+            this.btnKalansayisi = new System.Windows.Forms.Button();
+            this.btnGecensayisi = new System.Windows.Forms.Button();
+            this.btnAra100 = new System.Windows.Forms.Button();
+            this.btnBuyuktenkucuge = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnbuyuknot
+            // 
+            this.btnEnbuyuknot.BackColor = System.Drawing.Color.Honeydew;
+            this.btnEnbuyuknot.Location = new System.Drawing.Point(12, 465);
+            this.btnEnbuyuknot.Name = "btnEnbuyuknot";
+            this.btnEnbuyuknot.Size = new System.Drawing.Size(107, 49);
+            this.btnEnbuyuknot.TabIndex = 19;
+            this.btnEnbuyuknot.Text = "En Büyük Not";
+            this.btnEnbuyuknot.UseVisualStyleBackColor = false;
+            this.btnEnbuyuknot.Click += new System.EventHandler(this.btnEnbuyuknot_Click);
+            // 
+            // btnEndusuknot
+            // 
+            this.btnEndusuknot.BackColor = System.Drawing.Color.Honeydew;
+            this.btnEndusuknot.Location = new System.Drawing.Point(12, 410);
+            this.btnEndusuknot.Name = "btnEndusuknot";
+            this.btnEndusuknot.Size = new System.Drawing.Size(107, 49);
+            this.btnEndusuknot.TabIndex = 18;
+            this.btnEndusuknot.Text = "En Düşük Not";
+            this.btnEndusuknot.UseVisualStyleBackColor = false;
+            this.btnEndusuknot.Click += new System.EventHandler(this.btnEndusuknot_Click);
+            // 
+            // btnBelge
+            // 
+            this.btnBelge.BackColor = System.Drawing.Color.Honeydew;
+            this.btnBelge.Location = new System.Drawing.Point(12, 355);
+            this.btnBelge.Name = "btnBelge";
+            this.btnBelge.Size = new System.Drawing.Size(107, 49);
+            this.btnBelge.TabIndex = 17;
+            this.btnBelge.Text = "Teşekkür veya Takdir Sayısı";
+            this.btnBelge.UseVisualStyleBackColor = false;
+            this.btnBelge.Click += new System.EventHandler(this.btnBelge_Click);
+            // 
+            // btnKalansayisi
+            // 
+            this.btnKalansayisi.BackColor = System.Drawing.Color.Honeydew;
+            this.btnKalansayisi.Location = new System.Drawing.Point(12, 300);
+            this.btnKalansayisi.Name = "btnKalansayisi";
+            this.btnKalansayisi.Size = new System.Drawing.Size(107, 49);
+            this.btnKalansayisi.TabIndex = 16;
+            this.btnKalansayisi.Text = "Kalan Sayısı";
+            this.btnKalansayisi.UseVisualStyleBackColor = false;
+            this.btnKalansayisi.Click += new System.EventHandler(this.btnKalansayisi_Click);
+            // 
+            // btnGecensayisi
+            // 
+            this.btnGecensayisi.BackColor = System.Drawing.Color.Honeydew;
+            this.btnGecensayisi.Location = new System.Drawing.Point(12, 245);
+            this.btnGecensayisi.Name = "btnGecensayisi";
+            this.btnGecensayisi.Size = new System.Drawing.Size(107, 49);
+            this.btnGecensayisi.TabIndex = 15;
+            this.btnGecensayisi.Text = "Geçen Sayısı";
+            this.btnGecensayisi.UseVisualStyleBackColor = false;
+            this.btnGecensayisi.Click += new System.EventHandler(this.btnGecensayisi_Click);
+            // 
+            // btnAra100
+            // 
+            this.btnAra100.BackColor = System.Drawing.Color.Honeydew;
+            this.btnAra100.Location = new System.Drawing.Point(12, 190);
+            this.btnAra100.Name = "btnAra100";
+            this.btnAra100.Size = new System.Drawing.Size(107, 49);
+            this.btnAra100.TabIndex = 14;
+            this.btnAra100.Text = "100 Ara";
+            this.btnAra100.UseVisualStyleBackColor = false;
+            this.btnAra100.Click += new System.EventHandler(this.btnAra100_Click);
+            // 
+            // btnBuyuktenkucuge
+            // 
+            this.btnBuyuktenkucuge.BackColor = System.Drawing.Color.Honeydew;
+            this.btnBuyuktenkucuge.Location = new System.Drawing.Point(12, 135);
+            this.btnBuyuktenkucuge.Name = "btnBuyuktenkucuge";
+            this.btnBuyuktenkucuge.Size = new System.Drawing.Size(107, 49);
+            this.btnBuyuktenkucuge.TabIndex = 13;
+            this.btnBuyuktenkucuge.Text = "Büyükten Küçüğe";
+            this.btnBuyuktenkucuge.UseVisualStyleBackColor = false;
+            this.btnBuyuktenkucuge.Click += new System.EventHandler(this.btnBuyuktenkucuge_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Honeydew;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(12, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 49);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Küçükten Büyüğe";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Honeydew;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(231, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 537);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Honeydew;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 49);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Notları Oluştur";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(566, 645);
+            this.Controls.Add(this.btnEnbuyuknot);
+            this.Controls.Add(this.btnEndusuknot);
+            this.Controls.Add(this.btnBelge);
+            this.Controls.Add(this.btnKalansayisi);
+            this.Controls.Add(this.btnGecensayisi);
+            this.Controls.Add(this.btnAra100);
+            this.Controls.Add(this.btnBuyuktenkucuge);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnbuyuknot;
+        private System.Windows.Forms.Button btnEndusuknot;
+        private System.Windows.Forms.Button btnBelge;
+        private System.Windows.Forms.Button btnKalansayisi;
+        private System.Windows.Forms.Button btnGecensayisi;
+        private System.Windows.Forms.Button btnAra100;
+        private System.Windows.Forms.Button btnBuyuktenkucuge;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ orneksınav1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace orneksınav1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> notlar = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            notlar.Clear();
+            Random rnd = new Random();
+            for (int i = 1; i <= 38; i++)
+            {
+                notlar.Add(rnd.Next(0, 101));
+            }
+            listBox1.DataSource = notlar.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            notlar.Sort();
+            listBox1.DataSource = notlar.ToList();
+        }
+
+        private void btnBuyuktenkucuge_Click(object sender, EventArgs e)
+        {
+            notlar.Sort();
+            notlar.Reverse();
+
+            listBox1.DataSource = notlar.ToList();
+        }
+
+        private void btnAra100_Click(object sender, EventArgs e)
+        {
+            if (notlar.Contains(100) == true)
+            {
+                MessageBox.Show("100 alan öğrenci var.");
+            }
+            else
+            {
+                MessageBox.Show("100 alan öğrenci yok.");
+            }
+        }
+
+        private void btnGecensayisi_Click(object sender, EventArgs e)
+        {
+
+            int gecenSayisi = 0;
+            for (int i = 0; i < notlar.Count; i++)
+            {
+                if (notlar[i] >= 50)
+                {
+                    gecenSayisi++;
+                }
+            }
+            MessageBox.Show("Geçen Sayısı: " + gecenSayisi);
+        }
+
+        private void btnBelge_Click(object sender, EventArgs e)
+        {
+            int belgeSayisi = 0;
+            for (int i = 0; i < notlar.Count; i++)
+            {
+                if (notlar[i] >= 70)
+                {
+                    belgeSayisi++;
+                }
+            }
+            MessageBox.Show("Belge alan  Sayısı: " + belgeSayisi);
+        }
+
+        private void btnKalansayisi_Click(object sender, EventArgs e)
+        {
+            int kalanSayisi = 0;
+            for (int i = 0; i < notlar.Count; i++)
+            {
+                if (notlar[i] < 50)
+                {
+                    kalanSayisi++;
+                }
+            }
+            MessageBox.Show("kalan Sayısı: " + kalanSayisi);
+        }
+
+        private void btnEndusuknot_Click(object sender, EventArgs e)
+        {
+            notlar.Sort();
+            MessageBox.Show("En düşük Not: " + notlar[0]);
+        }
+
+        private void btnEnbuyuknot_Click(object sender, EventArgs e)
+        {
+            notlar.Sort();
+            notlar.Reverse();
+            MessageBox.Show("En Yüksek Not :" + notlar[0]);
+        }
+    }
+}
